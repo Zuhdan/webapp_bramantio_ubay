7bdbc7cab812339caeb02c0ffe75b798cee16ec1
Zuhdan Ubay <ubayresearch@gmail.com>
2017-04-23 11:06:30 +0700

    moving image

--- BRAMBAY/src/main/java/com/bramantio/ubay/webapp/project/homeVM.java
+++ BRAMBAY/src/main/java/com/bramantio/ubay/webapp/project/homeVM.java
@@ -1,11 +1,20 @@
 package com.bramantio.ubay.webapp.project;
 
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+
+import java.util.Date;
+
 /**
  * Hi!
  * Created by captain_n3mo on 23/04/2017.
  */
 public class homeVM {
 
+    private String name, email, tujuan;
+    private int phone;
+    private Date date, startHour, endHour;
 
 
 }
